1d6feb960b6c204428e5bd019007b38ebdedb41e
sea-1604 <santiago.enriquez.a@gmail.com>
2024-04-29 13:47:33 -0400

    main train and test for frame selection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from model import FrameSelect, Unet
+import tensorflow as tf
 import argparse
 from preprocess import splits 
 
@@ -16,16 +17,54 @@ def parseArguments():
 
 if __name__ == "__main__":
     args = parseArguments()
-    
 
+    video_size = (112, 112, 128, 1)
+    systole = FrameSelect(video_size)
+    dyastole = FrameSelect(video_size)
 
-model = FrameSelect((112, 112, 128, 1))
-model.build(input_shape= (100, 112, 112, 128, 1))
-model.compile(optimizer= 'adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    systole.build(input_shape= (8722, 112, 112, 128, 1))
+    dyastole.build(input_shape= (8722, 112, 112, 128, 1))
 
-model = Unet((112, 112, 1))
-model.build(input_shape= (100, 112, 112, 1))
-model.compile(optimizer= 'adam', loss='categorical_crossentropy', metrics=['accuracy']) #cambiar loss y puede que metric
-model.summary()
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
+    systole.compile(optimizer= optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
+    dyastole.compile(optimizer= optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
+    systole.summary()
+
+    early_stopping = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',  # La métrica a monitorizar
+        patience=3,  # Cuántas épocas sin mejora antes de detener
+        restore_best_weights=True,  # Restaura los mejores pesos encontrados durante el entrenamiento
+    )
+
+    ## Trainig the model
+    mask_train, mask_val, mask_test, ims_train, ims_val, ims_test, vids_train, vids_val, vids_test, labels_sys_train, labels_sys_val, labels_sys_test, labels_dyas_train, labels_dyas_val, labels_dyas_test = splits(args)
+
+    systole_history = systole.fit(x= vids_train, 
+                                y= labels_sys_train, 
+                                batch_size= 64, 
+                                epochs= 500, 
+                                validation_data=(vids_val, labels_sys_val),
+                                validation_batch_size= 64,  
+                                callbacks= [early_stopping], 
+                                verbose= 2)
+
+    dyastole_history = systole.fit(x= vids_train, 
+                                y= labels_dyas_train, 
+                                batch_size= 64, 
+                                epochs= 500, 
+                                validation_data=(vids_val, labels_dyas_val),
+                                validation_batch_size= 64, 
+                                callbacks= [early_stopping], 
+                                verbose= 2)
+
+    print(f"Systole train and validation loss: {[systole_history.history['loss'], systole_history.history['val_loss']]} \t Systole train and validation accuracy: {[systole_history.history['accuracy'], systole_history.history['val_accuracy']]} \n",
+        f"dyastole train and validation loss: {[dyastole_history.history['loss'], dyastole_history.history['val_loss']]} \t dyastole train and validation accuracy: {[dyastole_history.history['accuracy'], dyastole_history.history['val_accuracy']]}",
+        f"Epochs systole and dyastole: {[len(systole_history.epochs), len(dyastole_history.epochs)]}")
+
+    ## Test the model
+    sys_test_loss, sys_test_acc = systole.evaluate(x= vids_test, y= labels_sys_test, batch_size= 64, verbose= 2)
+    dyas_test_loss, dyas_test_acc = dyastole.evaluate(x= vids_test, y= labels_dyas_test, batch_size= 64, verbose= 2)
+
+    print(f"Systole test loss and accuracy: {[sys_test_loss, sys_test_acc]} \n Dyastole test loss and accuracy: {[dyas_test_loss, dyas_test_acc]}")
