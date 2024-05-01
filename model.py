c14583e78240a28d709f1968218451727d66656a
sea-1604 <santiago.enriquez.a@gmail.com>
2024-04-30 21:43:15 -0400

    U-Net should be finish

--- model.py
+++ model.py
@@ -55,110 +55,85 @@ class Unet(Model):
                                        ReLU(),
                                        Conv2D(32, 3, padding='SAME'),
                                        BatchNormalization(),
-                                       ReLU(),
-                                       MaxPool2D((2, 2))
+                                       ReLU()
          ])
+        self.pool_1 = MaxPool2D((2, 2), strides= 2)
         
         self.conv_block2 = Sequential([Conv2D(64, 3, padding='SAME'),
                                        BatchNormalization(),
                                        ReLU(),
                                        Conv2D(64, 3, padding='SAME'),
                                        BatchNormalization(),
-                                       ReLU(),
-                                       MaxPool2D((2, 2))
+                                       ReLU()
          ])
+        self.pool_2 = MaxPool2D((2, 2), strides= 2)
         
         self.conv_block3 = Sequential([Conv2D(128, 3, padding='SAME'),
                                        BatchNormalization(),
                                        ReLU(),
                                        Conv2D(128, 3, padding='SAME'),
                                        BatchNormalization(),
-                                       ReLU(),
-                                       MaxPool2D((2, 2))
-         ])
-        
-        self.center = Sequential([Conv2D(256, 3, padding='SAME'),
-                                       BatchNormalization(),
-                                       ReLU(),
-                                       Conv2D(256, 3, padding='SAME'),
-                                       BatchNormalization(),
                                        ReLU()
          ])
+        self.pool_3 = MaxPool2D((2, 2), strides= 2)
         
-        self.decoder_3 = Conv2DTranspose(128, 2, strides=2, padding='SAME')         
-        self.decoder_conv_3 = Sequential([BatchNormalization(),     # recives a concatenated input 
-                                          ReLU(),
-                                          Conv2D(128, 3, padding='SAME'), 
-                                          BatchNormalization(),
-                                          ReLU(),
-                                          Conv2D(128, 3, padding='SAME')
+        self.center = Sequential([Conv2D(256, 3, padding='SAME'),
+                                  BatchNormalization(),
+                                  ReLU(),
+                                  Conv2D(256, 3, padding='SAME'),
+                                  BatchNormalization(),
+                                  ReLU()
          ])
         
-        self.decoder_2 = Conv2DTranspose(64, 2, strides=2, padding='SAME')         
-        self.decoder_conv_2 = Sequential([BatchNormalization(),     # recives a concatenated input 
+        self.decoder_3 = Conv2DTranspose(128, 2, strides=2)         
+        self.decoder_conv_3 = Sequential([Conv2D(128, 3, padding='SAME'),
+                                          BatchNormalization(),     # recives a concatenated input 
                                           ReLU(),
-                                          Conv2D(64, 3, padding='SAME'), 
+                                          Conv2D(128, 3, padding='SAME'),
                                           BatchNormalization(),
-                                          ReLU(),
-                                          Conv2D(128, 3, padding='SAME')
+                                          ReLU()
          ])
         
-        self.decoder_1 = Conv2DTranspose(32, 2, strides=2, padding='SAME')         
-        self.decoder_conv_1 = Sequential([BatchNormalization(),     # recives a concatenated input 
+        self.decoder_2 = Conv2DTranspose(64, 2, strides=2)         
+        self.decoder_conv_2 = Sequential([Conv2D(64, 3, padding='SAME'),
+                                          BatchNormalization(),     # recives a concatenated input 
                                           ReLU(),
-                                          Conv2D(32, 3, padding='SAME'), 
+                                          Conv2D(64, 3, padding='SAME'),
                                           BatchNormalization(),
-                                          ReLU(),
-                                          Conv2D(32, 3, padding='SAME')
+                                          ReLU()
          ])
+        
+        self.decoder_1 = Conv2DTranspose(32, 2, strides=2)         
+        self.decoder_conv_1 = Sequential([Conv2D(32, 3, padding='SAME'),
+                                          BatchNormalization(),     # recives a concatenated input 
+                                          ReLU(),
+                                          Conv2D(32, 3, padding='SAME'),
+                                          BatchNormalization(),
+                                          ReLU()
+         ])
+        
+        self.out = Conv2D(1, 1, activation= 'sigmoid')
+        
         
     @tf.function
     def call(self, frames):
         
-        x, encoder_1 = self.conv_block1(frames)
-        x, encoder_2 = self.conv_block2(x)
-        x, encoder_3 = self.conv_block3(x)
+        encoder_1 = self.conv_block1(frames)
+        x = self.pool_1(encoder_1)
+        encoder_2 = self.conv_block2(x)
+        x = self.pool_2(encoder_2)
+        encoder_3 = self.conv_block3(x)
+        x = self.pool_3(encoder_3)
 
         x = self.center(x)
 
         x = self.decoder_3(x)
-        x = self.decoder_conv_3(tf.concat([x, encoder_3]))
+        x = self.decoder_conv_3(tf.concat([x, encoder_3], axis= -1))
         x = self.decoder_2(x)
-        x = self.decoder_conv_2(tf.concat([x, encoder_2]))
+        x = self.decoder_conv_2(tf.concat([x, encoder_2], axis= -1))
         x = self.decoder_1(x)
-        x = self.decoder_conv_1(tf.concat([x, encoder_1]))
+        x = self.decoder_conv_1(tf.concat([x, encoder_1], axis= -1))
+        
+        out = self.out(x)
 
-        return x
-
-
-"""
-def conv_block(input_tensor, num_filters):
-    encoder = layers.Conv3D(num_filters, (3, 3, 3), padding='same')(input_tensor)
-    encoder = layers.BatchNormalization()(encoder)
-    encoder = layers.PReLU()(encoder)
-    encoder = layers.Dropout(0.3)(encoder)  # Añade dropout
-    return layers.PReLU()(encoder)
-
-def encoder_block(input_tensor, num_filters):
-    encoder = conv_block(input_tensor, num_filters)
-    return encoder, layers.MaxPooling3D((2, 2, 2))(encoder)
-
-def decoder_block(input_tensor, concat_tensor, num_filters):
-    decoder = layers.Conv3DTranspose(num_filters, (2, 2, 2), strides=(2, 2, 2), padding='same')(input_tensor)
-    decoder = layers.concatenate([concat_tensor, decoder], axis=-1)
-    decoder = layers.BatchNormalization()(decoder)
-    decoder = layers.PReLU()(decoder)
-
-    return layers.Conv3D(num_filters, (3, 3, 3), padding='same')(decoder)
-
-
-def get_vnet(input_shape, num_classes):
-    inputs = tf.keras.Input(shape=input_shape)
-
-    encoder0, pool0 = encoder_block(inputs, 32)
-    
-    center = conv_block(pool0, 64)
-    
-    decoder0 = decoder_block(center, encoder0, 32)
-
-    outputs = layers.Conv3D(num_classes, (1, 1, 1), activation='softmax')(decoder0)"""
+        return out
